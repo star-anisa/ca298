5a806477b2e14e34dc21712681c89d33a2a1a2c9
root <root@Anisa>
2023-01-24 15:38:56 +0000

    Base code for lab two

--- Lab_1/labone/Anisa_shop/urls.py
+++ Lab_1/labone/Anisa_shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('index.html', views.index, name="index"),
-   path('contact.html', views.contact, name="contact")
+   path('', views.index, name="index"),
+   path('contact.html', views.contact, name="contact"),
+   path('index.html', views.index, name="homepage")
    ]
